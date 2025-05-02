5a55cc1abbe861118e4f893e0b552a4e0eb0ce92
Kerrion Siokwu <ladykerrion@sky.com>
2025-05-03 00:39:43 +0100

    refactor(contacts): implement full contact form handling
    - Added JSON file service utilities (read/write with error handling)
    - Added timestamps to form submissions
    
    The system now properly:
    1. Handles form submissions. 2.Tracks submission times

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as postController from '../controllers/postController.js';
 import * as viewController from '../controllers/viewController.js';
+import * as contactController from '../controllers/contactController.js';
 
 
 const router = express.Router();
@@ -18,10 +19,12 @@ router.get('/posts/:id', postController.getPostById); // Specific Post Page
 router.get('/posts/category/:category', postController.getPostsByCategory); // Posts by Category
 router.get('/posts/title/:title', postController.getPostByTitle); // Posts by Title
 
+
 // Form Submission Routes
 router.post('/posts/create', postController.createPost); // Create Post
 router.post('/posts/update/:id', postController.updatePost); // Update Post
 router.post('/posts/delete/:id', postController.deletePost); // Delete Post
+router.post('/contact', contactController.handleContactFormSubmission)
 
 export default router;
 
