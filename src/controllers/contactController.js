798becab98cb526590718d25e50a665fce4cb5e8
Kerrion Siokwu <ladykerrion@sky.com>
2026-03-11 18:15:22 +0000

    update contactController and subscriptionController

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -3,10 +3,39 @@ import * as contactModel from "../models/contactModel.js";
 // Function to handle contact form submission
 export async function handleContactFormSubmission(req, res) {
   try {
+    const {name, email, subject, message} = req.body;
+
+    //Validate required fields
+    if(!name || !email || !subject || !message) {
+      return res.status(400).render("contact.ejs", {
+        error: "All fields are required",
+        formData: req.body,
+        currentPage: "contact",
+        user: req.session.user || null
+      });
+    }
+
+    await contactModel.contact(name, email, subject, message);
+    res.render("thanks.ejs", {
+      currentPage: "thanks",
+      user: req.session.user || null
+    });
+
+  } catch (error) {
+    console.error("Error saving contact: ", error);
+    res.status(500).render("contact.ejs", { 
+      error: "Failed to send message. Please try again",
+      formData: req.body,
+      currentPage:"contact",
+      user: req.session.user || null
+    });
+  }
+  
+  /* try {
     await contactModel.handleContactFormSubmission(req.body); // Pass form data
     res.render('thanks.ejs');
   } catch (error) {
     console.error("Error saving contact: ", error);
     res.status(500).json({ success: false, message: "Failed to save message"})
-  }
+  } */
 }
--- src/controllers/subscriptionController.js
+++ src/controllers/subscriptionController.js
@@ -3,10 +3,36 @@ import * as subscriptionModel from "../models/subscriptionModel.js";
 // Function to handle contact form submission
 export async function handleSubscriberFormSubmission(req, res) {
   try {
+    const {email} = req.body;
+
+    // Validate email
+    if (!email) {
+      return res.status(400).render("footer.ejs", {
+        subscriptionError: "Email is required",
+        currentPage: req.body.currentPage,
+        user: req.session.user || null
+      });
+    }
+
+    await subscriptionModel.subcription(email);
+
+    // Redirect back to the page they are coming from with success message
+    res.redirect(`${req.get("referer") || "/"}?subscription=success`)
+  } catch (error) {
+    console.error("Error saving subscriber: ", error);
+
+    // Handle duplicate email error
+    if (error.code === "23505") {
+      return res.redirect(`${req.get("referer") || "/"}?subscription=duplicate`);
+    }
+    
+    res.redirect(`${req.get("referer") || "/"}?subscription=error`);
+  }
+  /* try {
     await subscriptionModel.handleSubscribeFormSubmission(req.body); // Pass form data
     res.render('thanks.ejs');
   } catch (error) {
     console.error("Error saving subscriber: ", error);
     res.status(500).json({ success: false, message: "Failed to subscribe"})
-  }
+  } */
 }
